8e690412efa5ef24563d086726a06557f7143c87
Lucas Bernadino <luucasdinoo@gmail.com>
2024-10-18 13:17:48 -0300

    feat: register page

---
+++ my-app/src/pages/cadastro/components/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TemplateFrame from './TemplateFrame';
+import RegisterCard from './RegisterCard';
+
+export default function RegisterPage() {
+  return (
+    <TemplateFrame>
+      <RegisterCard />
+    </TemplateFrame>
+  );
+}
