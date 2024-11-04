3ee6aeebd6ffde5c105f1d6aa04920407d7dc2a4
Lucas Bernadino <luucasdinoo@gmail.com>
2024-11-04 16:31:16 -0300

    feat: indice page

---
+++ my-app/src/pages/indice/IndicePage.js
@@ -0,0 +1,10 @@
+import TemplateFrame from "../cadastro/components/TemplateFrame";
+import IndiceCard from "./IndiceCard";
+
+export default function IndicePage(){
+    return (
+        <TemplateFrame>
+            <IndiceCard/>
+        </TemplateFrame>
+    );
+}
